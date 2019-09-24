74778952f70aacaf15bc59dc6138b566594d1aa2
ghost_tq <452260794@qq.com>
2019-09-24 12:48:51 +0800

    hohoho!

--- examples/iOS/BenchmarkSuite/BenchmarkSuite/BenchmarkAppDelegate.m
+++ examples/iOS/BenchmarkSuite/BenchmarkSuite/BenchmarkAppDelegate.m
@@ -13,7 +13,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor greenColor];
     
     mainTabBarController = [[UITabBarController alloc] initWithNibName:nil bundle:nil];
     
